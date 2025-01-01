dadd7c548b8ccc455ac0d4233e2ded97fa6668c6
Chrissie21 <chrisbalele21@gmail.com>
2025-01-01 21:57:08 +0300

    cpp

--- Hotel.cpp
+++ Hotel.cpp
@@ -13,3 +13,41 @@ struct Room {
 class HotelManagement {
 private:
     vector<Room> rooms;
+
+    public:
+    HotelManagement(int numRooms) {
+        for (int i = 1; i <= numRooms; ++i) {
+            rooms.push_back({i, (i % 2 == 0 ? "Double" : "Single"), false, ""});
+        }
+    }
+    void bookRoom(const string& guestName, const string& roomType) {
+        for (auto& room : rooms) {
+            if (!room.isOccupied && room.type == roomType) {
+                room.isOccupied = true;
+                room.guestName = guestName;
+                cout << "Room " << room.number << " booked for " << guestName << ".\n";
+                return;
+            }
+        }
+        cout << "No available " << roomType << " rooms.\n";
+    }
+        void checkOut(int roomNumber) {
+        for (auto& room : rooms) {
+            if (room.number == roomNumber) {
+                if (room.isOccupied) {
+                    room.isOccupied = false;
+                    room.guestName = "";
+                    cout << "Room " << roomNumber << " is now available.\n";
+                    return;
+                } else {
+                    cout << "Room " << roomNumber << " is already vacant.\n";
+                    return;
+                }
+            }
+        }
+        cout << "Invalid room number.\n";
+    }
+
+        void displayRooms() {
+        cout << "\nRoom Status:\n";
+        for (const auto& room : rooms) {
